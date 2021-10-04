d7ee1dacd4c48d7307777ae7b2fc43c875b5c05c
Seunghwan <ysp1307@naver.com>
2021-10-04 21:10:31 +0900

    initial firebase set

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNRdi08NpuiTiLiHrK3AOtW0Pp4526CA4",
+  authDomain: "twittercl-e7342.firebaseapp.com",
+  projectId: "twittercl-e7342",
+  storageBucket: "twittercl-e7342.appspot.com",
+  messagingSenderId: "595713017245",
+  appId: "1:595713017245:web:16a59968e31a8442911b74",
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
